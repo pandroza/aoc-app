2694473f99314184def272371ad934296e0b709f
pandroza <60747184+pandroza@users.noreply.github.com>
2022-05-30 00:24:47 +0100

    second app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+import 'css-doodle';
+
+function App() {
+  return (
+     <css-doodle use="var(--rule1)"id = "doodle"></css-doodle>
+  );
+}
+
+export default App;
